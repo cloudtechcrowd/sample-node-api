0b0d7f439af5d45415fa80e4b0160583af6ba490
Jesus Hilarion Roig Peix <cloudtechcrowd@gmail.com>
2023-04-21 23:29:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,172 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcryptjs')
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://samplebd:sample123@cluster0.jx8ceng.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to MongoDB!');
+    app.listen(3000, () => {
+        console.log(`started on port 3000`);
+    });
+});
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    gender: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/api/register', async (req, res) => {
+    try {
+        const { email, password, name, gender } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'Email already in use' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+            name,
+            gender
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({ message: 'User created' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.post('/api/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const token = jwt.sign({ email }, 'secretkey');
+        return res.status(200).json({ token });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+const jwt = require('jsonwebtoken');
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ users });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({ user });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const { email, password, name, gender } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+            name,
+            gender
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({ message: 'User created' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.put('/api/users/:id', async (req, res) => {
+    try {
+        const { email, password, name, gender } = req.body;
+
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.email = email || user.email;
+        user.name = name || user.name;
+        user.gender = gender || user.gender;
+        if (password) {
+            user.password = await bcrypt.hash(password, 10);
+        }
+
+        await user.save();
+
+        return res.status(200).json({ message: 'User updated' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.delete('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        await User.findByIdAndDelete(req.params.id);
+
+        return res.status(200).json({ message: 'User deleted' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
---
+++ models/user.js
@@ -0,0 +1,8 @@
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
